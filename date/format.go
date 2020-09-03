87984a57e5f6b3aa56d9b5a745370405ade066f8
yukiz97 <akayuki97@live.com>
2020-09-03 21:32:04 +0700

    update format

--- date/format.go
+++ date/format.go
@@ -4,6 +4,8 @@ import "time"
 
 //Format1 for the format of yyyy-MM-dd HH:mm:ss
 var Format1 string = "2006-01-02 15:04:05"
+//Format1 for the format of yyyy-MM-dd
+var Format2 string = "2006-01-02"
 
 //FormatTimeToString format a time with an format
 func FormatTimeToString(inputTime time.Time, format string) string {
